444f6b071119de8d9f2df275305e8f638613af72
frectonz <fraol0912@gmail.com>
2024-06-23 21:36:19 +0300

    add new tiles

--- src/IndianHistory.tsx
+++ src/IndianHistory.tsx
@@ -22,7 +22,7 @@ export const IndianHistory: React.FC<{}> = ({}) => {
   return (
     <AbsoluteFill
       style={{
-        backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='40' height='40' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0,0%,100%,1)'/><path d='M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5'  stroke-width='1' stroke='none' fill='hsla(0, 0%, 0%, 1)'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")`,
+        backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='20' height='20' patternTransform='scale(2) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0,0%,100%,1)'/><path d='M 10,-2.55e-7 V 20 Z M -1.1677362e-8,10 H 20 Z'  stroke-width='1' stroke='hsla(0, 0%, 0%, 1)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")`,
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
@@ -52,6 +52,109 @@ export const IndianHistory: React.FC<{}> = ({}) => {
             >
               Indian Architecture
             </h3>
+          </div>
+        </TransitionSeries.Sequence>
+
+        <TransitionSeries.Transition
+          presentation={slide({ direction: "from-right" })}
+          timing={linearTiming({ durationInFrames: 30 })}
+        />
+
+        <TransitionSeries.Sequence durationInFrames={1200}>
+          <div
+            style={{
+              width: "100vw",
+              height: "100vh",
+              display: "grid",
+              gridTemplateRows: "90% 10%",
+              gap: 30,
+              padding: "2rem",
+            }}
+          >
+            <div
+              style={{
+                backgroundColor: "black",
+                padding: "2rem",
+                border: "2px solid black",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                overflow: "hidden",
+              }}
+            >
+              <Video
+                src={staticFile("mohenjo-daro.mp4")}
+                style={{ width: "100%" }}
+              />
+            </div>
+
+            <div
+              style={{
+                backgroundColor: "white",
+                padding: "2rem",
+                boxShadow: "10px 10px black",
+                border: "2px solid black",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <h3 className="tillana-bold" style={{ fontSize: 30 }}>
+                Mohenjo-daro
+              </h3>
+            </div>
+          </div>
+        </TransitionSeries.Sequence>
+
+        <TransitionSeries.Transition
+          presentation={slide({ direction: "from-right" })}
+          timing={linearTiming({ durationInFrames: 30 })}
+        />
+
+        <TransitionSeries.Sequence durationInFrames={650}>
+          <div
+            style={{
+              width: "100vw",
+              height: "100vh",
+              display: "grid",
+              gridTemplateRows: "90% 10%",
+              gap: 30,
+              padding: "2rem",
+            }}
+          >
+            <div
+              style={{
+                backgroundColor: "black",
+                padding: "2rem",
+                border: "2px solid black",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                overflow: "hidden",
+              }}
+            >
+              <Video src={staticFile("stupa.mp4")} style={{ width: "100%" }} />
+            </div>
+
+            <div
+              style={{
+                backgroundColor: "white",
+                padding: "2rem",
+                boxShadow: "10px 10px black",
+                border: "2px solid black",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <h3 className="tillana-bold" style={{ fontSize: 30 }}>
+                Buddhist Stupa
+              </h3>
+            </div>
           </div>
         </TransitionSeries.Sequence>
 
--- src/Root.tsx
+++ src/Root.tsx
@@ -7,7 +7,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="IndianHistory"
         component={IndianHistory}
-        durationInFrames={7300}
+        durationInFrames={9000}
         fps={30}
         width={1920}
         height={1080}
